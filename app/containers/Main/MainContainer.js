cfc29c34b3af4279c3efb192d5bdbe4a4096c926
Eric Fries <ericfries@gmail.com>
2016-12-05 11:09:56 -0500

    add reducers and react router

---
+++ app/containers/Main/MainContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainContainer = React.createClass({
+    render() {
+        return (
+            <p>Hi</p>
+        )
+    }
+})
+
+export default MainContainer;
